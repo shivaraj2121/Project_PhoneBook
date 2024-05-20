80411cb43c84e2b8ce715b94aa3cf87f30c3a6ed
shivaraj2121 <shivarajb525@gmail.com>
2024-05-20 11:07:10 +0530

    added custom exception

--- DataStructureClasses/addContact.ts
+++ DataStructureClasses/addContact.ts
@@ -1,18 +1,15 @@
-import ContactNotFoundError from "./error";
-
-
+import {ContactNotFoundError,ContactAlreadyExistsError,InvalidContactDataError} from "./error";
 export default class PhoneBook {
     contacts: any[];
     constructor() {
         this.contacts = [];
     }
-    addContact(name: string, phoneNumber: string ,alterPhonenumber:string) {
+    addContact(name: string, phoneNumber: any ,alterPhonenumber:any) {
         try{
             this.contacts.push({ name, phoneNumber ,alterPhonenumber});
             console.log(`Contact "${name}" added successfully.`);
         }catch(error){
-            throw new ContactNotFoundError(error.message)
-
+            throw new ContactAlreadyExistsError(error.message)
         }
     }
     updateContact(name: string, newPhoneNumber: string, alterNatenewPhoneNumberToAdd: string) {
@@ -21,30 +18,39 @@ export default class PhoneBook {
         if (contact) {
             contact.phoneNumber = newPhoneNumber;
             console.log(`Contact "${name}" updated successfully.`);
-        } else {
-            console.log(`Contact "${name}" not found.`);
-        }
-      }catch(error){
+        }if(contact)
+            {
+                contact.alterPhoneNumber=alterNatenewPhoneNumberToAdd;
+                console.log(`cotnact "${name}" updated successfully`)
+            }
+       }catch(error){
             throw new ContactNotFoundError(error.message)
-    }
+       }
     }
     deleteContact(name: string) {
+        try{
         
         const index = this.contacts.findIndex((contact: { name: any; }) => contact.name === name);
         if (index !== -1) {
             this.contacts.splice(index, 1);
             console.log(`Contact "${name}" deleted successfully.`);
-        } else {
-            console.log(`Contact "${name}" not found.`);
-        }
+        } 
+       }catch(error){
+            throw new InvalidContactDataError(error.message) 
+       }
     }
     displayContacts() {
-        console.log("Contacts:");
-        this.contacts.forEach((contact: { name: any; phoneNumber: any; alterPhoneNumber:any}) => {
+        try{
+            console.log("Contacts:");
+            this.contacts.forEach((contact: { name: any; phoneNumber: any; alterPhoneNumber:any}) => {
             console.log(`${contact.name}: ${contact.phoneNumber}:${contact.alterPhoneNumber}`);
         });
+        }catch(error){
+            throw new ContactNotFoundError(error.message)
+
+      }
     }
     findContact(name: any) {
-        return this.contacts.find((contact: { name: any; }) => contact.name === name);
+            return this.contacts.find((contact: { name: any; }) => contact.name === name);
     }
 }
